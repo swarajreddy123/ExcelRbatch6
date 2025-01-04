b5f8bb5bca595625dc00e53014ae6f1fda24121c
swarajreddy123 <reddyswaraj57@gmail.com>
2025-01-04 10:00:59 +0530

    accept the bank user details

---
+++ demo09.java
@@ -0,0 +1,16 @@
+public class demo09 {
+    public static void acceptDetails(String customerName, String accountNumber, double balance) {
+        System.out.println("Customer Name: " + customerName);
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Total Balance: " + balance);
+    }
+    public static void displayDetails() {
+        String customerName = "raju";
+        String accountNumber = "54747558885";
+        double balance = 10000.0;
+        acceptDetails(customerName, accountNumber, balance);
+    }
+    public static void main(String[] args) {
+        displayDetails();
+    }
+}
