23a6855bf60b6cb65bd131b92ab1395af94d83e3
swarajreddy123 <reddyswaraj57@gmail.com>
2025-01-23 12:44:48 +0530

    String to Integer (atoi) Implementation

---
+++ LTCode003.java
@@ -0,0 +1,45 @@
+public class LTCode003 {
+    public int myAtoi(String s) {
+        s = s.trim();
+        if (s.isEmpty()) {
+            return 0;
+        }
+        
+        int index = 0;
+        int sign = 1;
+        long result = 0;
+        
+        if (s.charAt(index) == '-') {
+            sign = -1;
+            index++;
+        } else if (s.charAt(index) == '+') {
+            index++;
+        }
+        
+        while (index < s.length() && Character.isDigit(s.charAt(index))) {
+            result = result * 10 + (s.charAt(index) - '0');
+            index++;
+            
+            if (result * sign > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+            if (result * sign < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+        }
+        
+        return (int) (result * sign);
+    }
+
+    public static void main(String[] args) {
+        LTCode003 solution = new LTCode003();
+        
+        System.out.println(solution.myAtoi("42"));
+        System.out.println(solution.myAtoi("   -42"));
+        System.out.println(solution.myAtoi("4193 with words"));
+        System.out.println(solution.myAtoi("words and 987"));
+        System.out.println(solution.myAtoi("-91283472332"));
+        System.out.println(solution.myAtoi("   +0 123"));
+        System.out.println(solution.myAtoi("21474836460"));
+    }
+}
