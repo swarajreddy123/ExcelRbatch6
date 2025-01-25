c9d2da183d99061b950b8b32a25d7f396cdd8919
swarajreddy123 <reddyswaraj57@gmail.com>
2025-01-25 12:24:15 +0530

    Swap Two Numbers Without Using a Temporary Variable

---
+++ Demo045.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Demo045 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt();
+
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+    }
+}
