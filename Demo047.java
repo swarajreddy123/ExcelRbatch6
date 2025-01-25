19dc6a9cb407d8af7281370c62f344f4d3f1b822
swarajreddy123 <reddyswaraj57@gmail.com>
2025-01-25 12:25:19 +0530

    Count the Number of Digits in an Integer

---
+++ Demo047.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Demo047 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();        
+        int count = 0;
+        while (number != 0) {
+            number /= 10;
+            count++;
+        }
+        System.out.println("Number of digits: " + count);
+    }
+}
