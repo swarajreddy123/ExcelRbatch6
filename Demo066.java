8f4ada77cf5efb8bfb718ff35ed219ecdeeaff6d
swarajreddy123 <reddyswaraj57@gmail.com>
2025-02-06 12:16:33 +0530

    a java program to print index values of an array

---
+++ Demo066.java
@@ -0,0 +1,10 @@
+public class Demo066 {
+    public static void main(String[] args) {
+        int[] numbers = {5, 10, 15, 20, 25, 30, 35};
+
+        System.out.println("Index values and array elements:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Index " + i + " -> " + numbers[i]);
+        }
+    }
+}
