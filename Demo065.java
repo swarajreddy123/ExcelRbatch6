25772517c992f33dd15f446104aaa58286e04001
swarajreddy123 <reddyswaraj57@gmail.com>
2025-02-06 12:12:44 +0530

    Write a java program to print array elements

---
+++ Demo065.java
@@ -0,0 +1,10 @@
+public class Demo065 {
+    public static void main(String[] args) {
+        int[] numbers = {5, 10, 15, 20, 25, 30, 35};
+
+        System.out.println("Array elements:");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+    }
+}
