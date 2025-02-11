2123b54ed92146bdd58c4ed75bc1d662dea93618
swarajreddy123 <reddyswaraj57@gmail.com>
2025-02-11 12:24:20 +0530

    sort an array without predefined methods Accenture

---
+++ Demo074.java
@@ -0,0 +1,18 @@
+public class Demo074 {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 1, 2};
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.print("Sorted array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
